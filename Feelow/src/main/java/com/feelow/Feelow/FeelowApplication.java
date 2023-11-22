efce32c28cd7713fe778b37c7295184773e684e8
narong <criticalpxint@naver.com>
2023-11-22 10:50:24 +0900

    initial commit

---
+++ Feelow/src/main/java/com/feelow/Feelow/FeelowApplication.java
@@ -0,0 +1,13 @@
+package com.feelow.Feelow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FeelowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FeelowApplication.class, args);
+	}
+
+}
---
+++ Feelow/src/test/java/com/feelow/Feelow/FeelowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.feelow.Feelow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FeelowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
