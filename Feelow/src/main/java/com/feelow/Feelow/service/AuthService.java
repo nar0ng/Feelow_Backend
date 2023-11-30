03ca27dcb9741d5f8807e337694a955c24cb365a
narong <criticalpxint@naver.com>
2023-12-01 01:57:25 +0900

    Feat: jwt 토큰 생성

--- Feelow/src/main/java/com/feelow/Feelow/service/AuthService.java
+++ Feelow/src/main/java/com/feelow/Feelow/service/AuthService.java
@@ -4,6 +4,7 @@ import com.feelow.Feelow.dto.SignUpDto;
 import com.feelow.Feelow.dto.ResponseDto;
 import com.feelow.Feelow.domain.Member;
 import com.feelow.Feelow.dto.SignInResponseDto;
+import com.feelow.Feelow.jwt.TokenProvider;
 import com.feelow.Feelow.repository.MemberRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -16,8 +17,15 @@ public class AuthService {
     @Autowired
     MemberRepository memberRepository;
 
+    @Autowired
+    TokenProvider tokenProvider;
+
     public ResponseDto<?> signUp(SignUpDto dto) {
         Long id = dto.getId();
+        String email = dto.getEmail();
+
+        String token = tokenProvider.create(email);
+        int exprTime = 3600000;
 
 
         try {
@@ -27,7 +35,7 @@ public class AuthService {
             if (existingMemberOptional.isPresent()) {
                 // ID가 이미 존재하면, 기존 회원 정보를 반환
                 Member existingMember = existingMemberOptional.get();
-                SignInResponseDto signInResponseDto = new SignInResponseDto(null, 0, existingMember);  // Token과 exprTime은 null 및 0으로 초기화
+                SignInResponseDto signInResponseDto = new SignInResponseDto(token, exprTime, existingMember);  // Token과 exprTime은 null 및 0으로 초기화
                 return ResponseDto.setSuccess("Already existing member", signInResponseDto);
             } else {
                 /// 존재하지 않는 ID인 경우, Member 엔티티 생성 및 저장
@@ -41,6 +49,7 @@ public class AuthService {
             // 기타 예외 처리
             return ResponseDto.setFailed("Error", null);
         }
+
 
     }
 }
