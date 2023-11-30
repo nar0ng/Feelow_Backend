ffc9c6085ed89ae48ed5683a29e4de958bbc6f7d
narong <criticalpxint@naver.com>
2023-11-30 11:04:09 +0900

    Refactor: 회원정보 db에 저장-> 회원가입 처리

---
+++ Feelow/src/main/java/com/feelow/Feelow/domain/Member.java
@@ -0,0 +1,61 @@
+package com.feelow.Feelow.domain;
+
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.feelow.Feelow.dto.SignUpDto;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+import java.util.Map;
+
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity(name="Member")
+@Table(name="Member")
+public class Member {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long member_id;
+
+    @JsonProperty("id")
+    private Long id;
+
+    @JsonProperty("connected_at")
+    private LocalDateTime connected_at;
+
+    private String nickname;
+
+    private String email;
+
+    @SuppressWarnings("unchecked")
+    @JsonProperty("properties")
+    private void unpackNested_p(Map<String,Object> properties) {
+        this.nickname = (String)properties.get("nickname");
+    }
+
+    @SuppressWarnings("unchecked")
+    @JsonProperty("kakao_account")
+    private void unpackNested_k(Map<String,Object> kakao_account) {
+        this.email = (String)kakao_account.get("email");
+    }
+
+    public Member(SignUpDto dto){
+        this.member_id = dto.getMember_id();
+        this.id = dto.getId();
+        this.connected_at = dto.getConnected_at();
+        this.email = dto.getEmail();
+        this.nickname = dto.getNickname();
+
+
+    }
+
+}
+
+
--- Feelow/src/main/java/com/feelow/Feelow/repository/MemberRepository.java
+++ Feelow/src/main/java/com/feelow/Feelow/repository/MemberRepository.java
@@ -1,7 +1,8 @@
 package com.feelow.Feelow.repository;
 
-import com.feelow.Feelow.entity.Member;
+import com.feelow.Feelow.domain.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 
-public interface MemberRepository extends CrudRepository<Member, Long> {
+public interface MemberRepository extends JpaRepository<Member, Long> {
 }
