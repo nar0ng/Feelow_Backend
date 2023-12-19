6e6d0862f3042c837c03903073e68421b6bd33b3
narong <criticalpxint@naver.com>
2023-12-20 03:06:25 +0900

    ._ 수정

--- Feelow/src/main/java/com/feelow/Feelow/jwt/TokenProvider.java
+++ Feelow/src/main/java/com/feelow/Feelow/jwt/TokenProvider.java
@@ -1,41 +1,130 @@
 package com.feelow.Feelow.jwt;
-
 import io.jsonwebtoken.Claims;
-import io.jsonwebtoken.Jwt;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
-import lombok.Data;
-import org.springframework.stereotype.Service;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.stereotype.Component;
 
 import java.time.Instant;
 import java.time.temporal.ChronoUnit;
 import java.util.Date;
 
-@Service
-public class TokenProvider {
+
+@Slf4j
+@Component
+public class TokenProvider implements InitializingBean {
+
+    // Refresh token expiration time: 1주일
+    private static final long REFRESH_TOKEN_EXPIRATION = 604800000;
+
     // jwt 생성 및 검증을 위한 키 생성
     private  static final String SECURITY_KEY = "jwtseckey!@";
 
     // jwt 생성하는 메서드
-    public String create (String email){
+    public String create (String email, String nickname){
         // 만료 기한 : 현재 시간 + 1시간
         Date exprTime = Date.from(Instant.now().plus(1, ChronoUnit.HOURS));
 
+        // 리프레시 토큰 만료 기한: 현재 시간 + 1주일
+        Date refreshExprTime = Date.from(Instant.now().plus(REFRESH_TOKEN_EXPIRATION, ChronoUnit.MILLIS));
+
+        // 리프레시 토큰 생성
+        String refreshToken = Jwts.builder()
+                .setSubject(email)
+                .setExpiration(refreshExprTime)
+                .signWith(SignatureAlgorithm.HS512, SECURITY_KEY)
+                .compact();
+
         // jwt 생성
         return Jwts.builder()
-                // 암호화에 사용될 알고리즘과 키
                 .signWith(SignatureAlgorithm.HS512, SECURITY_KEY)
-                // jwt 제목, 생성일, 만료일
-                .setSubject(email).setIssuedAt(new Date()).setExpiration(exprTime)
-                // 생성
+                .setSubject(email)
+                .claim("nickname", nickname)
+                .setIssuedAt(new Date())
+                .setExpiration(exprTime)
+                .claim("refreshToken", refreshToken)  // 리프레시 토큰을 클레임에 추가
                 .compact();
     }
 
     // jwt 검증
     public String validate(String token) {
+        try{
         // 매개변수로 받은 token을 디코딩
         Claims claims = Jwts.parser().setSigningKey(SECURITY_KEY).parseClaimsJws(token).getBody();
-        // 복호화된 토큰의 payload에서 subject를 가져옴
+
+        // 추가 검증
+        validateExpiration(claims);
+
+        // 유효한 경우 subject 반환
         return claims.getSubject();
+        } catch (Exception e) {
+            // 검증 실패 시 null 반환 또는 예외 처리
+            throw new BadCredentialsException("JWT validation failed", e);
+        }
     }
+
+    public MemberInfo getMemberInfo(String token) {
+        Claims claims = Jwts.parser().setSigningKey(SECURITY_KEY).parseClaimsJws(token).getBody();
+        String email = claims.getSubject();
+        String nickname = claims.get("nickname", String.class); 
+        return new MemberInfo(email, nickname);
+    }
+
+    // Refresh token
+    public String refresh(String accessToken){
+        try {
+            Claims claims = Jwts.parser().setSigningKey(SECURITY_KEY).parseClaimsJws(accessToken).getBody();
+
+            // 유효성 확인
+            validateExpiration(claims);
+
+            // 리프레시 토큰 갱신
+            Date newExpirationTime = Date.from(Instant.now().plus(REFRESH_TOKEN_EXPIRATION, ChronoUnit.MILLIS));
+            String refreshToken = claims.get("refreshToken", String.class);
+
+            // 리프레시 토큰이 존재하면 기존 리프레시 토큰을 사용하여 갱신
+            if (refreshToken != null) {
+                return Jwts.builder()
+                        .setClaims(claims)
+                        .setExpiration(newExpirationTime)
+                        .signWith(SignatureAlgorithm.HS512, SECURITY_KEY)
+                        .compact();
+            } else {
+                throw new RuntimeException("No existing refreshToken found for refresh");
+            }
+        } catch (Exception e) {
+            // 토큰 갱신 실패 시 예외 처리
+            log.error("Token refresh failed", e);
+            throw new BadCredentialsException("Token refresh failed", e);
+        }
+    }
+
+    @Override
+    public void afterPropertiesSet() throws Exception {
+
+    }
+
+    private void validateExpiration(Claims claims) {
+        // 토큰의 만료 시간 가져오기
+        Date expiration = claims.getExpiration();
+        // 현재 시간과 비교하여 만료 여부 확인
+        if (expiration != null && expiration.before(new Date())) {
+            throw new BadCredentialsException("JWT has expired");
+        }
+    }
+    // TokenProvider 클래스 내에 추가할 메서드
+    public boolean isAccessTokenExpired(String accessToken) {
+        try {
+            Claims claims = Jwts.parser().setSigningKey(SECURITY_KEY).parseClaimsJws(accessToken).getBody();
+            return claims.getExpiration().before(new Date());
+        } catch (Exception e) {
+            return true; // 만료 여부 확인 중 에러 발생 시도 만료된 것으로 처리
+        }
+    }
+
 }
+
+
+
--- Feelow/src/main/java/com/feelow/Feelow/service/AuthService.java
+++ Feelow/src/main/java/com/feelow/Feelow/service/AuthService.java
@@ -3,8 +3,7 @@ package com.feelow.Feelow.service;
 import com.feelow.Feelow.dto.SignUpDto;
 import com.feelow.Feelow.dto.ResponseDto;
 import com.feelow.Feelow.domain.Member;
-import com.feelow.Feelow.dto.SignInResponseDto;
-import com.feelow.Feelow.dto.SignUpResponseDto;
+import com.feelow.Feelow.dto.MemberResponseDto;
 import com.feelow.Feelow.jwt.TokenProvider;
 import com.feelow.Feelow.repository.MemberRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -25,8 +24,7 @@ public class AuthService {
     public ResponseDto<?> signUp(SignUpDto dto) {
         Long id = dto.getId();
         String email = dto.getEmail();
-
-
+        String nickname = dto.getNickname();
 
         try {
             // 이미 존재하는 ID인지 확인
@@ -34,28 +32,47 @@ public class AuthService {
 
             if (existingMemberOptional.isPresent()) {
 
-                String token = tokenProvider.create(email);
-                int exprTime = 3600000;
-
                 // ID가 이미 존재하면, 기존 회원 정보를 반환
                 Member existingMember = existingMemberOptional.get();
-                SignInResponseDto signInResponseDto = new SignInResponseDto(token, exprTime, existingMember);
-                return ResponseDto.setSuccess(HttpStatus.OK, "Already existing member", signInResponseDto);
+
+                // 토큰 생성
+                String accessToken = tokenProvider.create(email, nickname);
+                String refreshToken = tokenProvider.refresh(accessToken); // refreshToken 생성
+                int exprTime = 3600000; // 1시간
+
+
+                // 만료된 accessToken인 경우 refreshToken을 사용하여 새로운 accessToken 발급
+                if (tokenProvider.isAccessTokenExpired(accessToken)) {
+                    accessToken = tokenProvider.refresh(refreshToken);
+                }
+
+                // 갱신된 토큰 및 만료 시간을 응답 DTO에 담아 반환
+                MemberResponseDto memberResponseDto = new MemberResponseDto(accessToken, refreshToken, exprTime, existingMember);
+                return ResponseDto.success(HttpStatus.OK, "Already existing member", memberResponseDto);
             } else {
 
-                String token = tokenProvider.create(email);
-                int exprTime = 3600000;
-
                 /// 존재하지 않는 ID인 경우, Member 엔티티 생성 및 저장
-                 Member newMember = new Member(dto);
+                Member newMember = new Member(dto);
                 memberRepository.save(newMember);
 
-                SignUpResponseDto signUpResponseDto = new SignUpResponseDto(token, exprTime, newMember);
-                return ResponseDto.setSuccess(HttpStatus.CREATED, "Sign up Success", signUpResponseDto);
+                // 토큰 생성
+                String accessToken = tokenProvider.create(email, nickname);
+                String refreshToken = tokenProvider.refresh(accessToken); // refreshToken 생성
+                int exprTime = 3600000; // 1시간
+                //int exprTime = 300000; // 5분 (test)
+
+
+                // 만료된 accessToken인 경우 refreshToken을 사용하여 새로운 accessToken 발급
+                if (tokenProvider.isAccessTokenExpired(accessToken)) {
+                    accessToken = tokenProvider.refresh(refreshToken);
+                }
+
+                MemberResponseDto memberResponseDto = new MemberResponseDto(accessToken, refreshToken, exprTime, newMember);
+                return ResponseDto.success(HttpStatus.CREATED, "Sign up Success", memberResponseDto);
             }
         } catch (Exception e) {
             // 기타 예외 처리
-            return ResponseDto.setFailed(HttpStatus.BAD_GATEWAY, "Error", null);
+            return ResponseDto.failed(HttpStatus.BAD_GATEWAY, "Error", null);
         }
 
 
