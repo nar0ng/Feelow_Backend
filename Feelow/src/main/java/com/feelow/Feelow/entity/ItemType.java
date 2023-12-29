e29d50f5dab10aacb1585b1ebe8d7c9a94ac93b9
psy-er <89902211+psy-er@users.noreply.github.com>
2023-12-29 12:05:34 +0900

    itemType controller, dto, entity 완성

--- Feelow/src/main/java/com/feelow/Feelow/entity/ItemType.java
+++ Feelow/src/main/java/com/feelow/Feelow/entity/ItemType.java
@@ -1,4 +1,32 @@
 package com.feelow.Feelow.entity;
 
+import com.feelow.Feelow.dto.ItemTypeDto;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Getter
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity
+@Table(name = "ItemType")
 public class ItemType {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long typeId;
+    @Column
+    private String title;
+
+    public static ItemType createItemType(ItemTypeDto itemtypeDto){
+        if(itemtypeDto.getTypeId() != null)
+            throw new IllegalArgumentException("생성 실패! id가 없어야 합니다.");
+        return new ItemType(
+                itemtypeDto.getTypeId(),
+                itemtypeDto.getTitle()
+        );
+
+    }
+
 }
