6a11548d307b6e634675990acc3cddaf0a1a0bec
narong <criticalpxint@naver.com>
2024-01-19 00:09:44 +0900

    Feat: 선생님 마이페이지 기능 구현

--- Feelow/src/main/java/com/feelow/Feelow/controller/MyPageController.java
+++ Feelow/src/main/java/com/feelow/Feelow/controller/MyPageController.java
@@ -1,9 +1,11 @@
 package com.feelow.Feelow.controller;
 
 import com.feelow.Feelow.domain.Student;
+import com.feelow.Feelow.domain.Teacher;
 import com.feelow.Feelow.dto.MyPageDto;
 import com.feelow.Feelow.dto.ResponseDto;
 import com.feelow.Feelow.repository.StudentRepository;
+import com.feelow.Feelow.repository.TeacherRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -13,17 +15,27 @@ import java.util.Optional;
 
 @CrossOrigin(origins = "*", allowedHeaders = "*")
 @RestController
-@RequestMapping("/api/{studentId}")
+@RequestMapping("/api")
 @AllArgsConstructor
 public class MyPageController {
 
     private final StudentRepository studentRepository;
 
-    @GetMapping("/mypage")
-    public ResponseDto<MyPageDto> getMyPageInfo(@PathVariable Long studentId) {
+    private final TeacherRepository teacherRepository;
+
+    @GetMapping("/{studentId}/mypage/student")
+    public ResponseDto<MyPageDto> getStudentMyPageInfo(@PathVariable Long studentId) {
         Optional<Student> getStudent = studentRepository.findByStudentId(studentId);
 
         return getStudent.map(student -> ResponseDto.success("MyPage info found", MyPageDto.fromStudent(student)))
+                .orElse(ResponseDto.failed("MyPage info not found", null));
+    }
+
+    @GetMapping("/{teacherId}/mypage/teacher")
+    public ResponseDto<MyPageDto> getTeacherMyPageInfo(@PathVariable Long teacherId) {
+        Optional<Teacher> getTeacher = teacherRepository.findByTeacherId(teacherId);
+
+        return getTeacher.map(teacher -> ResponseDto.success("MyPage info found", MyPageDto.fromTeacher(teacher)))
                 .orElse(ResponseDto.failed("MyPage info not found", null));
     }
 }
