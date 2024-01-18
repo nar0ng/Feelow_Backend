0e77c7cefabb924eb9681988dad5707c63759065
narong <criticalpxint@naver.com>
2024-01-18 18:51:09 +0900

    Feat: 챗봇 채팅 기능 구현

--- Feelow/src/main/java/com/feelow/Feelow/FeelowApplication.java
+++ Feelow/src/main/java/com/feelow/Feelow/FeelowApplication.java
@@ -1,27 +1,29 @@
 package com.feelow.Feelow;
 
+import com.feelow.Feelow.dto.ResponseDto;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.autoconfigure.security.servlet.UserDetailsServiceAutoConfiguration;
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.RestTemplate;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @SpringBootApplication(exclude = {UserDetailsServiceAutoConfiguration.class})
 public class FeelowApplication {
-
 	public static void main(String[] args) {
+
 		SpringApplication.run(FeelowApplication.class, args);
+
 	}
 
-//	@Bean
-//	public WebMvcConfigurer corsConfigure(){
-//		return new WebMvcConfigurer() {
-//			@Override
-//			public void addCorsMappings(CorsRegistry registry) {
-//				registry.addMapping("/**").allowOriginsPatterns();
-//			}
-//
-//		};
-//	}
 }
+
