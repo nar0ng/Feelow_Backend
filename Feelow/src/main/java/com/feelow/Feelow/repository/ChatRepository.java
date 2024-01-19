e32f534339f8c12f99fda597c2f28df653f2d6b7
narong <criticalpxint@naver.com>
2024-01-19 18:47:07 +0900

    Refact: chat studentId->memberId로 변경

--- Feelow/src/main/java/com/feelow/Feelow/repository/ChatRepository.java
+++ Feelow/src/main/java/com/feelow/Feelow/repository/ChatRepository.java
@@ -6,5 +6,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface ChatRepository extends JpaRepository<Chat, Long> {
-    List<Chat> findByStudentStudentIdAndDate(Long studentId, String date);
+    // List<Chat> findByStudentStudentIdAndDate(Long studentId, String date);
+
+    List<Chat> findByMemberMemberIdAndDate(Long memberId, String date);
 }
