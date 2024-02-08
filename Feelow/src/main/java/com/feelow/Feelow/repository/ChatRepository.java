8eb288373c51740c4c8978a82e26f18be119b579
nar0ng <71819594+nar0ng@users.noreply.github.com>
2024-02-08 17:31:31 +0900

    :bug: FirstChat member 오류 수정 3

--- Feelow/src/main/java/com/feelow/Feelow/repository/ChatRepository.java
+++ Feelow/src/main/java/com/feelow/Feelow/repository/ChatRepository.java
@@ -5,9 +5,10 @@ import com.feelow.Feelow.domain.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface ChatRepository extends JpaRepository<Chat, Long> {
     List<Chat> findByMemberMemberIdAndDate(Long memberId, String date);
 
-    Member findByMemberMemberId(Long memberId);
+
 }
