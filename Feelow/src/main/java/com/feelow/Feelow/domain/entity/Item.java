2dde988f3c2473c1f3317369ed93f7be36925348
psy-er <89902211+psy-er@users.noreply.github.com>
2024-02-20 19:51:08 +0900

    Item API 완성

--- Feelow/src/main/java/com/feelow/Feelow/domain/entity/Item.java
+++ Feelow/src/main/java/com/feelow/Feelow/domain/entity/Item.java
@@ -1,5 +1,6 @@
 package com.feelow.Feelow.domain.entity;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import com.feelow.Feelow.domain.dto.ItemCreateRequest;
 import com.feelow.Feelow.domain.embedded.Money;
 import jakarta.persistence.*;
@@ -8,9 +9,10 @@ import lombok.*;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
 
-@Table(name = "Item")
+
 @Builder
-@Entity
+@Table(name = "Item")
+@Entity(name = "Item")
 @NoArgsConstructor
 @AllArgsConstructor
 @Getter
@@ -18,27 +20,34 @@ import org.hibernate.annotations.UpdateTimestamp;
 public class Item {
 
     @Id
+    @JsonProperty("itemId")
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "itemId", unique = true, nullable = false)
     private Long itemId;
 
+    @JsonProperty("name")
     @Column(length =50, nullable = false)
     private String name;
 
+    @JsonProperty("typeId")
     @Column(nullable = false)
     private Long typeId;
 
+    @JsonProperty("intro")
     @Column(length = 200, nullable = false)
     private String intro;
 
+    @JsonProperty("price")
     @Column(nullable = false)
     private Money price;
 
     @CreationTimestamp
+    @JsonProperty("created_at")
     @Column(name = "created_at", nullable = false, updatable = false)
     private LocalDateTime createdAt;
 
     @UpdateTimestamp
+    @JsonProperty("updated_at")
     @Column(name = "updated_at", nullable = false)
     private LocalDateTime updateAt;
 
