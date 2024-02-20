4e59864e977291e72e841fcb65f0c53a7a188268
psy-er <89902211+psy-er@users.noreply.github.com>
2024-02-20 20:27:56 +0900

    column 어노테이션 삭제

--- Feelow/src/main/java/com/feelow/Feelow/domain/entity/Item.java
+++ Feelow/src/main/java/com/feelow/Feelow/domain/entity/Item.java
@@ -21,33 +21,26 @@ public class Item {
     @Id
     @JsonProperty("itemId")
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "itemId", unique = true, nullable = false)
     private Long itemId;
 
     @JsonProperty("name")
-    @Column(length =50, nullable = false)
     private String name;
 
     @JsonProperty("typeId")
-    @Column(nullable = false)
     private Long typeId;
 
     @JsonProperty("intro")
-    @Column(length = 200, nullable = false)
     private String intro;
 
     @JsonProperty("price")
-    @Column(nullable = false)
     private Money price;
 
     @CreationTimestamp
     @JsonProperty("created_at")
-    @Column(name = "created_at", nullable = false, updatable = false)
     private LocalDateTime createdAt;
 
     @UpdateTimestamp
     @JsonProperty("updated_at")
-    @Column(name = "updated_at", nullable = false)
     private LocalDateTime updateAt;
 
 
