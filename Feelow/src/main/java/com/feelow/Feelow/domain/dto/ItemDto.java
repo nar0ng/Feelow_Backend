818e9cd500a8c80d4a032dbe6e0b78385ece4fb1
psy-er <89902211+psy-er@users.noreply.github.com>
2024-02-23 00:58:28 +0900

    Item 수정

--- Feelow/src/main/java/com/feelow/Feelow/domain/dto/ItemDto.java
+++ Feelow/src/main/java/com/feelow/Feelow/domain/dto/ItemDto.java
@@ -17,7 +17,8 @@ public class ItemDto {
     private String name; // 아이템 이름
     private Long typeId; // ItemType 외래키
     private String intro; // 아이템 소개
-    private Money price; // 아이템 가격
+    //private Money price; // 아이템 가격
+    private Long price;
     private LocalDateTime createdAt;
     private LocalDateTime updateAt;
 
